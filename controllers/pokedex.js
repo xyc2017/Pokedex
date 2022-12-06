30fe5abaccc902e7907c215ed574f5aa73793b10
Sabrina Chen <sabrinaxchen2016@gmail.com>
2022-12-05 22:19:40 -0800

    added delete route

--- controllers/pokedex.js
+++ controllers/pokedex.js
@@ -17,17 +17,32 @@ router.post('/', (req, res)=>{
     res.redirect('/pokemons')
 })
 
-// edit pokemon
+//edit pokemon
 router.get('/:id/edit', (req, res)=>{
-    res.render('edit.ejs', {
-        pokemon:pokemons[req.params.id],    
+    res.render('/views/edit.ejs', {
+        pokemon:pokemons[req.params.id]    
     })
 })
 
+// router.get('/:id', (req, res)=>{
+//     // go and get animal from the database
+//    Animals.findById(req.params.id)
+//    //find the animal and send it to the edit.ejs to prepopulate the form
+//    .then((animal)=>{
+//     res.render('animals/show.ejs', {animal})
+//     })
+// })
+
 // add show route
 router.get('/:id',(req, res)=>{
-    res.render('show.ejs', {pokemon:pokemons[req.params.id]})
+    res.render('show.ejs', {pokemon:pokemons[req.params.id]} 
+    )
    
+})
+
+router.delete('/:id', (req, res)=>{
+   for (let i=0; i<=pokemons.length; i++){pokemons.splice(pokemons[i],1)}
+    res.redirect('/pokemons')
 })
 
 module.exports=router
